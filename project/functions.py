604ae4361d37306a5462a067a863270e81a2c062
Dmitry Spitsyn <spitsynda@gmail.com>
2023-03-23 14:37:25 +0700

    final push

--- project/functions.py
+++ project/functions.py
@@ -12,7 +12,6 @@ hotel_API_KEY = os.getenv("hotels_API_KEY")
 
 
 def log(message, command, city, hotels):
-
     # Logs the user queries based on their telegram id
     if message.from_user.id not in history.log:
         history.log[message.from_user.id] = dict()
@@ -20,7 +19,6 @@ def log(message, command, city, hotels):
 
 
 def hotel_request(message):
-
     # Sends a request to hotel API using the personal parameters from history.data
     url = "https://hotels4.p.rapidapi.com/locations/v3/search"
     querystring = {"q": history.data[message.from_user.id]["city"]}
@@ -77,20 +75,17 @@ def hotel_request(message):
 
 
 def hotel_compute(data):
-
     # Makes a dictionary of hotel names and their ids for fetching the photos
     hotels = dict()
 
     for cur_hotel in data:
         hotels[cur_hotel.get('name')] = get_pictures(cur_hotel.get("id"))
-        # hotel['price'] = hotel_price(cur_hotel)
 
     return hotels
 
 
 def get_pictures(hotel_id):
-
-    # Requests hotel pictures through hotels api
+    # Requests hotel pictures through hotels api. Also gets address. Don't ask.
     url = "https://hotels4.p.rapidapi.com/properties/v2/detail"
 
     payload = {
@@ -108,6 +103,8 @@ def get_pictures(hotel_id):
 
     response = requests.request("POST", url, json=payload, headers=headers)
     hotel_data = response.json()
+    hotel_address = hotel_data.get("data").get("propertyInfo").get("summary").get("location").get("address").get(
+        "addressLine")
     hotel_data = hotel_data.get("data").get("propertyInfo").get("propertyGallery").get("images")
     hotel_pics = []
 
@@ -115,11 +112,10 @@ def get_pictures(hotel_id):
         pic = hotel_data[count].get("image").get("url")
         hotel_pics.append(pic)
 
-    return hotel_pics
+    return [hotel_pics, hotel_address]
 
 
 def fetch_city(message):
-
     # Gets the city from message, puts it in history.data, goes to get number of hotels for lowprice and highprice,
     # goes to fetch the price range for bestdeal command
     try:
@@ -137,7 +133,6 @@ def fetch_city(message):
 
 
 def fetch_hotel_num(message):
-
     # Gets the number of hotels, stores it in history.data, forwards to photo check
     try:
         hotel_num = int(message.text)
@@ -150,8 +145,8 @@ def fetch_hotel_num(message):
     except Exception:
         bot.send_message(message.from_user.id, "Something went wrong, start over or refer to /help command.")
 
-def bestdeal_price_range(message):
 
+def bestdeal_price_range(message):
     # Allows manual entry for price range if the chosen command is bestdeal. Forwards to the usual loop.
     data = message.text.split()
     try:
@@ -164,7 +159,6 @@ def bestdeal_price_range(message):
 
 
 def photo_check(message):
-
     # Checks if photos need to be posted. If True, then requests the number, if False - goes straight to results.
     if message.text.lower() == "yes":
         history.data[message.from_user.id]["photo_check"] = 1
@@ -178,7 +172,6 @@ def photo_check(message):
 
 
 def photo_num(message):
-
     # Store the number of photos that need to be posted in history.data. Forwards to results.
     try:
         history.data[message.from_user.id]["photo_count"] = int(message.text)
@@ -193,7 +186,6 @@ def photo_num(message):
 
 
 def photo_check_and_post(message):
-
     # Uses the information from history.data to form and send a request to hotel api through hotel_data function.
     # Logs the query results into history.log for future use in /history command. Posts the results to user.
     try:
@@ -201,36 +193,49 @@ def photo_check_and_post(message):
         hotel_data = hotel_request(message)
         hotel_list = hotel_compute(hotel_data)
         hotels = list(hotel_list.keys())
+        distance_and_price = distance_price(hotel_data)
+        inside_counter = 0
         log(message, command=history.data[message.from_user.id]["command"],
             city=history.data[message.from_user.id]["city"], hotels=hotels)
         for key, value in hotel_list.items():
-            bot.send_message(message.from_user.id, key)
+            bot.send_message(message.from_user.id,
+                             f"{key} - {distance_and_price[inside_counter][1]}\n{distance_and_price[inside_counter][0]} miles from city centre\n{value[1]}")
+            inside_counter += 1
             if history.data[message.from_user.id]["photo_check"] == 1:
                 for i in range(history.data[message.from_user.id]["photo_count"]):
-                    bot.send_photo(message.from_user.id, photo=value[i])
+                    bot.send_photo(message.from_user.id, photo=value[0][i])
     except Exception as ex:
         print(ex)
         bot.send_message(message.from_user.id, "Something went wrong, start over.")
 
 
-def lowprice(message):
+def distance_price(data):
+    # Makes a dictionary of distance to center and pricing
+    dist_price = []
 
+    for cur_hotel in data:
+        dist_price.append([cur_hotel.get("destinationInfo").get("distanceFromDestination").get("value"),
+                           cur_hotel.get("price").get("lead").get("formatted")])
+    return dist_price
+
+
+def lowprice(message):
     # Changes sorting method for lowest price possible
-    history.data[message.from_user.id] = {"command": "lowprice", "sort": "PRICE_LOW_TO_HIGH", "maxprice": 10000, "minprice": 10}
+    history.data[message.from_user.id] = {"command": "lowprice", "sort": "PRICE_LOW_TO_HIGH", "maxprice": 10000,
+                                          "minprice": 10}
     bot.send_message(message.from_user.id, "Which city are we searching in?")
     bot.register_next_step_handler(message, fetch_city)
 
 
 def highprice(message):
-
     # Changes sorting method for highest price possible
-    history.data[message.from_user.id] = {"command": "highprice", "sort": "PRICE_HIGH_TO_LOW", "maxprice": 10000, "minprice": 10}
+    history.data[message.from_user.id] = {"command": "highprice", "sort": "PRICE_HIGH_TO_LOW", "maxprice": 10000,
+                                          "minprice": 10}
     bot.send_message(message.from_user.id, "Which city are we searching in?")
     bot.register_next_step_handler(message, fetch_city)
 
 
 def bestdeal(message):
-
     # Changes sorting method to distance from the city center. Missing price arguments compared to other commands
     # since those are added by user further down the line.
     history.data[message.from_user.id] = {"command": "bestdeal", "sort": "DISTANCE"}
@@ -240,21 +245,21 @@ def bestdeal(message):
 
 
 def histor(message):
-
     # Weird function name due to conflict with the history object from History class that is used to store data.
     # Didn't think this through. Pulls the history of user queries based on their telegram id from history.log.
     bot.send_message(message.from_user.id, "History of your requests:")
     try:
         if message.from_user.id in history.log:
             for key, value in history.log[message.from_user.id].items():
-                bot.send_message(message.from_user.id, f"{key} - {value[0]} in {value[1]}. Hotels found: {', '.join(value[2])}")
+                bot.send_message(message.from_user.id,
+                                 f"{key} - {value[0]} in {value[1]}. Hotels found: {', '.join(value[2])}")
         else:
             bot.send_message(message.from_user.id, "Your history is empty")
     except Exception:
         bot.send_message(message.from_user.id, "Something went wrong, start over.")
 
-def helper(message):
 
+def helper(message):
     # Quick info on available commands
     bot.send_message(message.from_user.id, "Please use\n/lowprice for hotels with the lowest price in a given "
                                            "city\n/highprice for hotels with the highest price\n/bestdeal for the "
